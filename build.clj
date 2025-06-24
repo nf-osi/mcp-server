f7214c9ed57073125672ca9a08459ec2805728e3
Anh Nguyet Vu <anngvu@gmail.com>
2025-06-24 11:40:24 -0700

    Set up deps, build, etc.

---
+++ build.clj
@@ -0,0 +1,63 @@
+(ns build
+  (:refer-clojure :exclude [test])
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'nfosi/mcp-server)
+(def version "0.1.0")
+(def main 'nfosi.mcp-server)
+(def class-dir "target/classes")
+(def uber-file (format "target/%s-%s.jar" lib version))
+
+(def basis (delay (b/create-basis {:project "deps.edn"})))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn test "Run all the tests." [opts]
+  (let [basis    (b/create-basis {:aliases [:test]})
+        cmds     (b/java-command
+                  {:basis     basis
+                   :main      'clojure.main
+                   :main-args ["-m" "cognitect.test-runner"]})
+        {:keys [exit]} (b/process cmds)]
+    (when-not (zero? exit) (throw (ex-info "Tests failed" {}))))
+  opts)
+
+(defn- uber-opts [opts]
+  (assoc opts
+         :lib lib 
+         :main main
+         :uber-file uber-file
+         :basis (b/create-basis {})
+         :class-dir class-dir
+         :src-dirs ["src"]
+         :ns-compile [main]))
+
+(defn ci "Run the CI pipeline of tests (and build the uberjar)." [opts]
+  (test opts)
+  (b/delete {:path "target"})
+  (let [opts (uber-opts opts)]
+    (println "\nCopying source...")
+    (b/copy-dir {:src-dirs ["resources" "src"] :target-dir class-dir})
+    (println (str "\nCompiling " main "..."))
+    (b/compile-clj opts)
+    (println "\nBuilding JAR...")
+    (b/uber opts))
+  opts)
+
+(defn uber
+  "Build uberjar with prebuilt graph"
+  [opts]
+  (clean nil)
+  (println "Building uberjar...")
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/compile-clj {:basis @basis
+                  :ns-compile '[nfosi.mcp-server]
+                  :class-dir class-dir})
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis @basis
+           :main main})
+  (println "✓ Uberjar built successfully at" uber-file))
+
