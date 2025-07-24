d48a0186a0fe6d150ffa5acf50e95fcc4c51b4ab
Anh Nguyet Vu <anngvu@gmail.com>
2025-07-24 14:54:25 -0700

    Update prompt

--- src/nfosi/prompts.clj
+++ src/nfosi/prompts.clj
@@ -6,42 +6,54 @@
 
 
 ;; =============================================================================
-;; Project/data management prompts
+;; Project management prompts
 ;; =============================================================================
 
-(defn help-address-check-issues-handler 
-  [{:keys [issue]}]
-  (let [config-url "https://raw.githubusercontent.com/Sage-Bionetworks/data_curator_config/refs/heads/prod/NF-OSI/dca_config.json"]
-    {:messages [{:role "user"
-                 :content {:type "text"
-                           :text (str "Please look at GitHub issue " issue ", which should contain a report with check results for a Synapse project. "
-                                      "The report assesses datasets that need to pass metadata checks before they can be released as data artifacts and be usable in various apps/analysis downstream. "
-                                      "As my assistant, focus on the latest report results to help me resolve any check issues: "
-                                      "Review the report carefully to understand which datasets have complete and correct metadata, partial or incorrect metadata, or no metadata at all. " 
-                                      "If you need to go through the Synapse project in addition to the report (and the tools are available), you can do so. "
-                                      "For datasets with partial/incorrect metadata, generate a manifest and get as csv using our org's prod configuration below, so you have a local copy to work with. "
-                                      "Use our NF data model to research possible corrections, but create an issue in the data model repo if it's really an update to the data model that needs to be made in some cases. "
-                                      "If immediate corrections are possible, show Python code that can be applied to correct the manifest. "
-                                      "For datasets lacking metadata entirely, also generate a manifest with the appropriate template -- but for the data contributor to make the first pass at these. "
-                                      "Lastly, compose a friendly email to the contributor summarizing the state of their metadata, any corrections you were able to make, links to all manifests you have prepared for them, and anything pertinent to the check. "
-                                      "Let me know if you can't retrieve the GitHub resources because you don't have the tools available/lack permissions/report is missing. "
-                                      "Also let me know if anything seems noteworthy or if I can provide additional context. ")}}
-                 {:role "user"
-                  :content {:type "resource"
-                            :resource {:uri config-url
-                                       :text (slurp config-url)}}}
-                ]}))
-
-(registry/defprompt :help-address-check-issues
-  "Workflow for addressing items in project check report"
-   [{:name "issue"
-     :description "GitHub issue for project"
-     :required true}]
-   :category #{}
-   :permissions #{:read}
-   :handler help-address-check-issues-handler)
 
 
 ;; =============================================================================
 ;; Curation prompts
 ;; =============================================================================
+
+;; Agentic data curation / QC
+
+(defn curate-dataset-in-synapse
+  [{:keys [project_id]}]
+  (let [dataset-schema-url "https://repo-prod.prod.sagebase.org/repo/v1/schema/type/registered/org.synapse.nf-portaldataset"
+        dsp-url (str "https://dsp.nf.synapse.org/api/dsp/json/" project_id)]
+    {:messages [{:role "user"
+                 :content {:type "text"
+                           :text (str "As my highly competent and responsible assistant, your goal is to curate datasets within a Synapse project, which means creating Dataset entities and creating metadata for them that complies with our model. " 
+                                      ;; "and adding them to our staging dataset catalogue. " 
+                                      "Review the folders and files in project " project_id ", any existing Datasets, and the data sharing plan for that project if it exists. " 
+                                      "If it conceptually makes sense to create a dataset out of the group of files within a folder or set of folders, do so without unnecessary duplication of any existing Datasets. "
+                                      "Referencing the data sharing plan during curation is helpful for further context and matching expectations with reality. "
+                                      "Once you have a new Dataset entity, bind the JSON schema (also documented below) to it to facilitate organization-specific validation within our system. "
+                                      "The newly returned Dataset entity is a table that allows you to query it to understand the content and formulate informative metadata. "
+                                      "Once you have sufficient understanding, create and commit metadata for the Dataset that corresponds to our JSON schema. "
+                                      "Always check that your metadata submission was deemed valid after committing and make revisions as needed. "
+                                      "The definition of done is when all Datasets that conceptually makes sense have been created with valid metadata. "
+                                      "During this process, feel free to bring up issues to my attention, ask about additional tools you may need, and consult me on complex edge cases. ")}}
+                {:role "user"
+                 :content {:type "resource"
+                           :resource {:uri dataset-schema-url
+                                      :text (slurp dataset-schema-url)}}}
+                {:role "user"
+                 :content {:type "resource"
+                           :resource {:uri dsp-url
+                                      :text (slurp dsp-url)}}}
+                ]}))
+
+(registry/defprompt :curate-dataset-in-synapse
+  "Agentic workflow for curating datasets in Synapse"
+  [{:name "project_id"
+    :description "Synapse id for project"
+    :required true}]
+  :category #{}
+  :permissions #{:read}
+  :handler curate-dataset-in-synapse)
+
+;; TODO: (defn curate-dataset-external)
+
+;; (registry/defprompt :curate-dataset-external
+;;  "Workflow for curating datasets outside of Synapse"
